3fa4cbe8e8b3f53104199c6eed94eca6bc8e3011
K4rl1t0s <carlo.nicolo@hotmail.com>
2024-11-29 20:40:05 +0100

    lab4 for C++ and Python

---
+++ Daniel/C++/lab4.cpp
@@ -0,0 +1,23 @@
+/*
+Level of difficulty
+Easy
+
+Objectives
+Familiarize the student with:
+
+Using operators,
+Building simple expressions,
+Translating verbal description into programming language
+Scenario
+Take a look at the code we've provided in the editor: it assigns two integer values, manipulates them and finally outputs the result and bigresult variables.
+
+The problem is that the manipulations have been described using natural language, so the code is completely useless now.
+
+We want you to act as an intelligent (naturally!) compiler and to translate the formula into a real "C" code notation.
+
+Test your code using the data we've provided.
+
+Expected output
+result: 38
+big result: 54872
+*/
