3fa5ea34fadbd372447b3925313a995ce4484b24
Daniel Salvador <daniel.salvador@rosabianca.tn.it>
2024-12-05 17:44:40 +0100

    esercizio lab1 completato

--- Daniel/C/Exercise/lab1.c
+++ Daniel/C/Exercise/lab1.c
@@ -20,39 +20,44 @@ int main()
     // Acquisisco la grandezza del array da parte del utente
 
     int grandezza;
-
-    int check = 1;
-
+    int check;
+    do
+    {
     printf("Inserisci il numero di massima grandezza dell'array che intendi creare : ");
     scanf("%d", &grandezza);
     if (grandezza > 20)
     {
-        printf("L'array può essere grande al massimo di 20\n ");
+        printf("L'array può contenere al massimo 20 elementi!\n\n");
+        check = 0;
     }
     else
     {
-        printf("Cominciamo!\n");
+        printf("Cominciamo!\n\n");
+        check = 1;
     }
+}
+while (check == 0);
 
-    int array[grandezza];
+int array[grandezza];
 
-    // Chiedo di inserire i valori al utente e li inserisco nel array
+// Chiedo di inserire i valori al utente e li inserisco nel array
 
-    for (int i = 0; i <= grandezza-1; i++)
+for (int i = 0; i <= grandezza - 1; i++)
+{
+    printf("Inserisci il %d° valore :", i + 1);
+    scanf("%d", &array[i]);
+}
+
+for (int j = 0; j <= grandezza; j++)
+{
+
+    if (j % 2 == 0 && j >= 1)
     {
-        printf("Inserisci il %d° valore :", i);
-        scanf("%d", &array[i]);
+        printf("%d ", array[j]);
     }
+}
 
-    for (int j = 0; j <= grandezza-1; j++)
-    {
+printf("\n");
 
-        int c = array[j];
-        if (array[j] % 2 == 0)
-        {
-            printf("%d - ", c);
-        }
-    }
-
-    return 0;
+return 0;
 }
